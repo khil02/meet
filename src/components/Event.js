ce74ee5df7ccba64d801266b9c31016179217209
khil02 <120340087+khil02@users.noreply.github.com>
2023-10-19 01:24:47 -0400

    Updated CSS and event
    Added conversion to local time from UTC.

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,13 +4,17 @@ import { useState } from "react";
 
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
+  //this should convert the UTC time to standard American layout and local time
+  let [Y, M, D, H, m, s] = event.created.split(/\D/);
+  let date = new Date(Date.UTC(Y, M - 1, D, H, m, s));
+  date = date.toLocaleString();
 
   return (
     <li className="event">
       <div>
         <h2 className="eventTitle">{event.summary}</h2>
         <div className="location">{event.location}</div>
-        <div className="startTime">{event.created}</div>
+        <div className="startTime">{date}</div>
         <button
           className="detailsButton"
           onClick={() => {
